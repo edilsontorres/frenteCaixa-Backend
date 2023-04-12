1f0086245eb7f3653ef35a38197364cfe78a711f
Edilson Torres <edilsontor77@gmail.com>
2023-04-12 09:14:22 -0300

    Sistema de Login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using projetoCaixa.Models;
+using projetoCaixa.Service;
+
+namespace projetoCaixa.Controllers
+{
+    
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(User request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.PasswordHash);
+
+            user.UserName = request.UserName;
+            user.PasswordHash = "";
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> login(User request)
+        {
+            var tokenService = TokenService.CreateToken;
+
+            if(user.UserName !=  request.UserName)  
+            {
+                return BadRequest("Usuário não existe!");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(request.PasswordHash, user.PasswordHash))
+            {
+                return BadRequest("Senha errada!");
+            }
+
+            string token = tokenService(user);
+            Console.WriteLine(token);
+
+            return Ok(token);
+        }
+
+    }
+}
---
+++ Service/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using projetoCaixa.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace projetoCaixa.Service
+{
+    public class TokenService
+    {
+
+        public TokenService(IConfiguration configuration)
+        {
+            IConfiguration _configuration;
+            _configuration = configuration;
+        }
+        public static string CreateToken(User user)
+        {
+            
+            IConfiguration _configuration;
+            _configuration = 
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value!);
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName!)
+                }
+                ),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = jwtHandler.CreateToken(tokenDescription);
+            return jwtHandler.WriteToken(token);
+        }
+    }
+}
