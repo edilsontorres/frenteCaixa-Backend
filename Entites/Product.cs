79e627db06ab7529bb09c488db4b604e976cf5cc
Edilson Torres <edilsontor77@gmail.com>
2023-04-29 16:53:01 -0300

    Criando classe de produto

---
+++ Entites/Product.cs
@@ -0,0 +1,14 @@
+﻿using projetoCaixa.Migrations;
+
+namespace projetoCaixa.Entites
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Descriao { get; set; }
+        public float Preco { get; set; }
+        public int Estoque { get; set; }
+        public int UserId { get; set; }
+        //public User? Users { get; set; }
+    }
+}
